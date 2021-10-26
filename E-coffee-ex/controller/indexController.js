c395ad1288911ae1f7af0e0bd8ac475dfee857c9
Guilherme Mantz <guimantz@hotmail.com>
2021-10-25 21:41:29 -0300

    iniciando integração sequelize

--- E-coffee-ex/controller/indexController.js
+++ E-coffee-ex/controller/indexController.js
@@ -2,6 +2,9 @@ const indexController = {
     index: function(req, res){
         res.render('index');
     },
+    produtos: function(req, res) {
+        res.render('marcaSelecionada');
+    },
     logout: function(req, res) {
         req.session.destroy();
         res.redirect('/')
--- E-coffee-ex/routes/index.js
+++ E-coffee-ex/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var indexController = require('../controller/indexController');
 
 router.get('/', indexController.index);
+router.get('/cafeteiras', indexController.produtos);
 router.get('/logout', indexController.logout);
 
 module.exports = router;
