1cecc63d471e3c2e79278f7f5af7505a46ba119c
Guilherme Mantz <guimantz@hotmail.com>
2021-11-12 20:29:01 -0300

    criando migrations

---
+++ E-coffee-ex/database/migrations/001-clientes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('clientes', { 
+      id_cliente: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome: { 
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      sobrenome: { 
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      telefone: { 
+        type: Sequelize.STRING(11),
+        allowNull: false
+      },
+      cpf: { 
+        type: Sequelize.STRING.UUID(11),
+        allowNull: false,
+        
+      },
+      email: { 
+        type: Sequelize.STRING(80).UUID,
+        allowNull: false
+      },
+      senha: { 
+        type: Sequelize.STRING(100),
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('clientes');
+  }
+};
---
+++ E-coffee-ex/database/migrations/002-enderecos.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cli_enderecos', { 
+      id_cli_enderecos: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome_do_endereco: { 
+        type: Sequelize.STRING(45),
+        allowNull: false
+      },
+      logradouro: { 
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      cep: { 
+        type: Sequelize.STRING(8),
+        allowNull: false
+      },
+      num_residencia: { 
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        
+      },
+      bairro: { 
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      cidade: { 
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      uf: { 
+        type: Sequelize.STRING(2),
+        allowNull: false
+      },
+      complemento: { 
+        type: Sequelize.STRING(250),
+        allowNull: false
+      },
+      cliente: { 
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: { 
+          model: { 
+            tableName: 'clientes',
+          },
+          key: 'id_cliente'
+        }
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('enderecos');
+  }
+};
