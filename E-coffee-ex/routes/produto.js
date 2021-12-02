7d32a48277e12e687d209a2ce03e0534de9acdcc
RafaelBrunca <rafael.brunca@gmail.com>
2021-12-02 14:02:09 -0300

    Merge branch 'master' of https://github.com/Guilherme-Mantz/E-coffee

--- E-coffee-ex/routes/produto.js
+++ E-coffee-ex/routes/produto.js
@@ -2,12 +2,7 @@ var express = require('express');
 var router = express.Router();
 const produtoController = require('../controller/produtoController');
 
-<<<<<<< HEAD
 console.log('entrou aqui');
-=======
-router.get('/cafeteiras', produtoController.cafeteiras);
-router.get('/capsulas', produtoController.capsulas);
->>>>>>> d29acf02b1258db7e88ad34919ef364972720175
 router.get('/:id', produtoController.detalhes);
 router.get('/carrinho', produtoController.showCarrinho);
 module.exports = router;
